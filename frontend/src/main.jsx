7e361745aa5c8403dfcc4e14dc2c990c5fad6373
RoshanSharma7 <roshan.amlai96@gmail.com>
2026-06-03 23:41:19 +0530

    add and edit Background3D

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './styles/global.css'
-import Background3D from './three/Background3D.jsx'
+import Background3D from '../three/Background3D.jsx'
 import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
